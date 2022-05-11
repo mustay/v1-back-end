c917e68c4ca2e76f024f9379252df61dacf5a7b0
Mustafa Ahmad <ameermustafa92@gmail.com>
2022-05-12 03:24:06 +0500

    added upvote function

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const cors = require('cors')
+
+const { v4: uuidv4 } = require('uuid');
 const port = 3000;
 
 app.use(express.json());
@@ -18,8 +20,8 @@ impedit possimus.`;
 const users = ["Rob Hope", "Green Lantern", "Deadpool", "Shelby"];
 
 const comments = db.addCollection("comments");
-comments.insert({ user: "Rob Hope", time: "45 min ago", text });
-comments.insert({ user: "Green Lantern", time: "45 min ago", text });
+comments.insert({ id: uuidv4(), user: "Rob Hope", time: "45 min ago", text, upvotes: 0 });
+comments.insert({ id: uuidv4(), user: "Green Lantern", time: "45 min ago", text, upvotes: 1 });
 
 app.get("/comments", (req, res) => {
 	try {
@@ -30,21 +32,45 @@ app.get("/comments", (req, res) => {
 	}
 });
 
+app.patch("/comments", (req, res) => {
+	try {
+		const { id } = req.body;
+
+		console.log(req.body, id);
+		if (!id) {
+			res.statusCode = 400;
+			return res.send({
+				error: 'Missing params'
+			})
+		}
+		const comment = comments.by('id', id);
+		comment.upvotes++;
+		comments.update(comment);
+
+		return res.send({ message: "Upvote added"});
+
+	} catch (e) {
+		console.log(e.message);
+		return res.send({ error: e.message });
+	}
+})
+
 app.post("/comments", (req, res) => {
 	try {
-		console.log(req);
 		const text = req.body?.text;
 		if (!text) {
 			res.statusCode = 400;
 			return res.send({
-				message: "Missing text",
+				error: "Missing text",
 			});
 		}
 
 		const comment = {
 			user: users[Math.floor(Math.random() * users.length)],
 			text,
-			time: '45 min ago'
+			time: '45 min ago',
+			upvotes: 0,
+			id: uuidv4()
 		};
 		comments.insert(comment);
 		return res.send({
